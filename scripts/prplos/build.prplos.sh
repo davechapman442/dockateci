399ecb6599fd3834e9c820c0933dc6e94e145e67
Dave Chapman <dave.chapman@consultateci442.com>
2025-05-21 13:02:16 +0100

    Support for prplos 3.2.0 Banana Pi r3

---
+++ scripts/prplos/build.prplos.sh
@@ -0,0 +1,69 @@
+
+# MIT License
+# 
+# Copyright (c) 2025 Dave Chapman
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# Build runes for prplOS (3.2.0) for Banana Pi R3; R4 support coming in prplOS (4.1.0)
+# We'll turn this into a makefile with variables, defaults, warnings and perhaps input if not set!
+
+# More useful info here: https://holisticon.pl/blog/how-to-set-up-prplos-on-banana-pi-r3-a-step-by-step-guide/
+
+# You don't necessary want to run this script, but good for reference
+
+# Clone the prplOS repo
+git clone git@gitlab.com:prpl-foundation/prplos/prplos.git
+# You must have shared your public key with gitlab
+# You host .ssh folder will be available (bind mounted) into the dockateci container home
+# If it works on the host, then it should work here!
+cd prplos
+
+# Checkout out the 3.2.0 release
+git checkout 3.2.0
+# Copy in the Banana Pi profile definitions
+# R4 due to work in prpl OS 4.1.0 (in theory)
+# R3 functional in prpl OS 3.2.0
+cp ../*.yml ./profiles
+
+# Check the Banana Pi R3 profile is available
+./scripts/gen_config.py list | grep banana
+
+# Configure the build (bpi R3 target, with prpl OS base, debug build)
+./scripts/gen_config.py bpi_r3 prpl debug  
+
+# When finished you can check the result .config is correct
+cat .config | grep mediatek | grep -v '#'
+
+# Which should be:
+# CONFIG_TARGET_mediatek=y
+# CONFIG_TARGET_mediatek_filogic=y
+# CONFIG_TARGET_mediatek_filogic_DEVICE_bananapi_bpi-r3=y
+# CONFIG_TARGET_BOARD="mediatek"
+
+# And now build:
+make -j$(nproc)
+
+# Or simply make, if you don't want to fully occupy your machine/build machine
+# In case of failures, a more verbose take be invoked thus:
+make -j1 V=sc
+
+# If successful build files are found thus (as with OpenWRT):
+ls -1 ./bin/targets/mediatek/filogic
+
---
+++ scripts/rdk/build.rdkv.sh
@@ -0,0 +1,45 @@
+
+# MIT License
+# 
+# Copyright (c) 2025 Dave Chapman
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# Build runes for RDK-B (2025q1) for the Banana Pi (ref platform) NAND build (SDCARD at some point)
+# We'll turn this into a makefile with variables, defaults, warnings and perhaps input if not set!
+
+# You don't necessary want to run this script, but good for reference
+
+# From: https://wiki.rdkcentral.com/pages/viewpage.action?spaceKey=RDK&title=Raspberry+Pi+4+RDK6.1+Release+Notes
+
+# NOTE: To build RDKV 6.1 you must be an RDK Licensee (https://wiki.rdkcentral.com/display/RDK/Licensees)
+# From RDK 7 onwards the Reference Board build (presently Raspberry Pi 4) will be fully Open Source
+
+# You must use your Personal Access Token (PAT) as your password
+
+# Grab the manifest
+repo init -u https://code.rdkcentral.com/r/manifests -b 6.1.0 -m rdkv-extsrc.xml
+repo sync --no-clone-bundle --no-tags 
+  
+MACHINE=raspberrypi4-64-rdk-android-hybrid source meta-cmf-raspberrypi/setup-environment
+  
+bitbake lib32-rdk-generic-hybrid-wpe-image
+  
+# To build tdk image
+bitbake lib32-rdk-generic-hybrid-wpe-tdk-image
